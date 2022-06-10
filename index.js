58d49301f54ce90fae755bd08b67ef6042fc1675
Jose Molina <josemolinaf22@gmail.com>
2022-06-10 13:55:25 -0600

    added patch

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const pieRepo = require("./repos/pieRepo");
 //use the express router object
 let router = express.Router();
 
+// configure middleware to support json data parsing in request object
+app.use(express.json());
+
 //create get to return a list of all pies
 router.get("/", function (req, res, next) {
   pieRepo.get(
@@ -71,6 +74,104 @@ router.get("/:id", function (req, res, next) {
     }
   );
 });
+
+router.post("/", function (req, res, next) {
+  pieRepo.insert(
+    req.body,
+    function (data) {
+      res.status(201).json({
+        status: 201,
+        statusText: "Created",
+        message: "new pie added",
+        data: data,
+      });
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+router.put("/:id", function (req, res, next) {
+  pieRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        //attempt to update the data
+        pieRepo.update(req.body, req.params.id, function (data) {
+          res.status(200).json({
+            status: 200,
+            statusText: "OK",
+            message: "Pie '" + req.params.id + "' updated.",
+            date: data,
+          });
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: "Not Found",
+          messsage: "The pie '" + req.params.id + "' could not be found. ",
+          error: {
+            code: "NOT_FOUND",
+            message: "The pie '" + req.params.id + "' could not be found. ",
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+router.delete("/:id", function (req, res, next) {
+  pieRepo.getById(
+    req.params.id,
+    function (data) {
+      if (data) {
+        //attempt to delete the data
+        pieRepo.delete(req.params.id, function (data) {
+          res.status(200).json({
+            status: 200,
+            statusText: "OK",
+            message: "The pie '" + req.params.id + "' is deleted.",
+            data: "Pie '" + req.params.id + "' deleted.",
+          });
+        });
+      } else {
+        res.status(404).json({
+          status: 404,
+          statusText: "Not Found",
+          messsage: "The pie '" + req.params.id + "' could not be found. ",
+          error: {
+            code: "NOT_FOUND",
+            message: "The pie '" + req.params.id + "' could not be found. ",
+          },
+        });
+      }
+    },
+    function (err) {
+      next(err);
+    }
+  );
+});
+
+router.patch("/:id", function (req, res, next) {
+  pieRepo.getById(req.params.id, function (data) {
+    if (data) {
+      //attempt to update the data
+      pieRepo.update(req.body, req.params.id, function (data) {
+        res.status(200).json({
+          status: 200,
+          statusText: "Ok",
+          message: "Pie '" + req.params.id + "' patched.",
+          data: data,
+        });
+      });
+    }
+  });
+});
+
 // configure router so all routes are prefixed with /api/v1
 app.use("/api/", router);
 
